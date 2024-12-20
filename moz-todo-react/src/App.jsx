9b7120d18b6c676f749e0ca23118b5d9e52d0c9f
vc22832 <vc22832@utexas.edu>
2024-12-20 00:33:17 -0500

    stuff

---
+++ moz-todo-react/src/App.jsx
@@ -0,0 +1,30 @@
+import Todo from "./components/Todo";
+import FilterButton from "./components/FilterButton";
+import Form from "./components/Form";
+function addTask(name){
+  alert(name);
+}
+function App(props) {
+  const taskList = props.tasks?.map((task) => <Todo id={task.id} name={task.name} completed={task.completed} key = {task.id} />);
+  return (
+    <div className="todoapp stack-large">
+      <h1>To-do List</h1>
+      <Form addTask = {addTask}/>
+      <div className="filters btn-group stack-exception">
+        <FilterButton name = "All"/>
+        <FilterButton name = "Active"/>
+        <FilterButton name = "Completed"/>
+      </div>
+      <h2 id="list-heading">3 tasks remaining</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading">
+        {taskList}
+
+      </ul>
+    </div>
+  );
+}
+
+export default App;
