5d9b9a6e65f04b093efe0d456e5a42d90788032c
vc22832 <vc22832@utexas.edu>
2024-12-21 02:07:40 -0500

    delete button

--- moz-todo-react/src/App.jsx
+++ moz-todo-react/src/App.jsx
@@ -1,11 +1,38 @@
 import Todo from "./components/Todo";
 import FilterButton from "./components/FilterButton";
 import Form from "./components/Form";
-function addTask(name){
-  alert(name);
-}
+import { useState } from "react";
+
 function App(props) {
-  const taskList = props.tasks?.map((task) => <Todo id={task.id} name={task.name} completed={task.completed} key = {task.id} />);
+  const [tasks, setTasks] = useState(props.tasks);
+  const taskList = tasks?.map((task) => <Todo id={task.id} name={task.name} completed={task.completed} key = {task.id} toggleTaskCompleted = {toggleTaskCompleted} deleteTask = {deleteTask}/>);
+  const numberOfTasks = taskList?.length;
+  const headingText = `${numberOfTasks} ${numberOfTasks > 1 ? "tasks" : "task"} remaining`;
+
+  function addTask(name){
+      if (name === "") return;
+      const newTask = {id : `todo-${numberOfTasks}`, name, completed: false};
+      setTasks([...tasks, newTask]);
+    }
+
+  function toggleTaskCompleted(id){
+    const updatedTasks = tasks.map ((task) => {
+      if (id === task.id){
+        return {...task, completed: !task.completed};
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+    console.log(tasks)
+  }
+  
+  function deleteTask(id){
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+    console.log(tasks);
+  }
+
+
   return (
     <div className="todoapp stack-large">
       <h1>To-do List</h1>
@@ -15,7 +42,7 @@ function App(props) {
         <FilterButton name = "Active"/>
         <FilterButton name = "Completed"/>
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading"> {headingText} </h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
